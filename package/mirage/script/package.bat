1d9c83c7517a69fe43331af46fd2ad4d6175d6b7
Edward Yang <qwbarch@gmail.com>
2024-03-19 00:05:18 -0400

    Fix SpawnOnPlayerDeath
    This now checks if the player is on a nav mesh before spawning a masked enemy

--- package/mirage/script/package.bat
+++ package/mirage/script/package.bat
@@ -3,6 +3,7 @@
 call ./build.bat
 mkdir "../bin/BepInEx/plugins"
 powershell Move-Item -Path "../bin/Mirage.dll" -Destination "../bin/BepInEx/plugins/Mirage.dll"
+powershell Move-Item -Path "../bin/Mirage.pdb" -Destination "../bin/BepInEx/plugins/Mirage.pdb"
 powershell Compress-Archive^
     -Force^
     -Path "../bin/BepInEx/plugins",^
