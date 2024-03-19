8871b5b7392a2ff3c8ee9ac36d89fee913275f92
Edward Yang <qwbarch@gmail.com>
2024-03-19 01:29:47 -0400

    Embed debug symbols

--- package/mirage/script/build.bat
+++ package/mirage/script/build.bat
@@ -4,4 +4,3 @@ dotnet build ../src/Mirage.fsproj
 rmdir /s /q "..\bin"
 mkdir "..\bin"
 move "..\src\bin\Debug\netstandard2.1\Mirage.dll" "..\bin\Mirage.dll"
-move "..\src\bin\Debug\netstandard2.1\Mirage.pdb" "..\bin\Mirage.pdb"
