7391e799026e5393476924391a4c3de3d8364a56
Edward Yang <qwbarch@gmail.com>
2024-03-31 00:56:21 -0400

    Reset mirage package

---
+++ lib/FSharpx.Async/README.md
@@ -0,0 +1,2 @@
+Note: This is a fork, located at https://github.com/qwbarch/FSharpx.Async  
+This should be removed for the official build when it gets merged to upstream.
--- package/mirage/src/Mirage/Plugin.fs
+++ package/mirage/src/Mirage/Plugin.fs
@@ -1,44 +1,10 @@
 namespace Mirage
 
-open Dissonance
-open UnityEngine
-open System
-open System.IO
 open BepInEx
-open FSharpPlus
-open HarmonyLib
-open Netcode
-open NAudio.Lame
 open Mirage.PluginInfo
-open Mirage.Core.Config
-open Mirage.Core.Logger
-open Mirage.Core.Audio.Recording
-open Mirage.Patch.NetworkPrefab
-open Mirage.Patch.SyncConfig
-open Mirage.Patch.RemovePenalty
-open Mirage.Patch.RecordAudio
-open Mirage.Patch.SpawnMaskedEnemy
 
-[<BepInPlugin(pluginName, pluginId, pluginVersion)>]
-//[<BepInDependency(LobbyCompatibility.PluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.SoftDependency)>]
+[<BepInPlugin(pluginId, pluginName, pluginVersion)>]
 type Plugin() =
     inherit BaseUnityPlugin()
 
-    let onError () = logError "Failed to initialize Mirage. Plugin is disabled."
-
-    member this.Awake() =
-        handleResultWith onError <| monad' {
-            Logs.SetLogLevel(LogCategory.Recording, LogLevel.Error);
-            initNetcodePatcher()
-            return! initConfig this.Config
-            ignore <| LameDLL.LoadNativeDLL [|Path.GetDirectoryName this.Info.Location|]
-            Application.add_quitting deleteRecordings
-            let harmony = new Harmony(pluginId)
-            iter (unbox<Type> >> harmony.PatchAll) 
-                [   typeof<RegisterPrefab>
-                    typeof<RecordAudio>
-                    typeof<SpawnMaskedEnemy>
-                    typeof<SyncConfig>
-                    typeof<RemovePenalty>
-                ]
-        }
+    member _.Awake() = ()
