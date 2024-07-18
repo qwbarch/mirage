f7b5de4fb5520b2648eda283edff5ac083f87fba
qwbarch <qwbarch@gmail.com>
2024-07-17 20:52:42 -0400

    Fix dependencies pointing to git lfs

--- lib/FSharpx.Async/README.md
+++ lib/FSharpx.Async/README.md
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2ef9b337410c0cfbfe47fe76014f2ab1e286afdbd2454cc6bc431622c3929589
-size 154
+Note: This is a fork, located at https://github.com/qwbarch/FSharpx.Async  
+This should be removed for the official build when it gets merged to upstream.
--- lib/onnxruntime/include/provider_options.h
+++ lib/onnxruntime/include/provider_options.h
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb7206ffd06ba31b9f7e5f6089a6206f0c720c10c3adcc61db7f829c86691cfc
-size 480
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace onnxruntime {
+
+// data types for execution provider options
+
+using ProviderOptions = std::unordered_map<std::string, std::string>;
+using ProviderOptionsVector = std::vector<ProviderOptions>;
+using ProviderOptionsMap = std::unordered_map<std::string, ProviderOptions>;
+
+}  // namespace onnxruntime
