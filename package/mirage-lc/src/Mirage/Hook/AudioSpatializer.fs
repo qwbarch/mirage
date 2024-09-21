64ec425d4409c825a3cddb3debd5c7e4765ce5c8
qwbarch <qwbarch@gmail.com>
2024-09-21 11:36:10 -0400

    Add AudioSpatializer hook

---
+++ package/mirage-lc/src/Mirage/Hook/AudioSpatializer.fs
@@ -0,0 +1,15 @@
+module Mirage.Hook.AudioSpatializer
+
+open FSharpPlus
+open UnityEngine
+
+/// Disables the audio spatializer log spam.
+/// Credits: [IAmBatby](https://github.com/IAmBatby) and [mattymatty](https://github.com/mattymatty97).
+let disableAudioSpatializer () =
+    On.Unity.Netcode.NetworkSceneManager.add_OnSceneLoaded(fun orig self eventId ->
+        orig.Invoke(self, eventId)
+        let pluginName = AudioSettings.GetSpatializerPluginName()
+        if isNull pluginName || pluginName = zero then
+            let disableSpatialize (audioSource: AudioSource) = audioSource.spatialize <- false
+            iter disableSpatialize <| Resources.FindObjectsOfTypeAll<AudioSource>()
+    )
