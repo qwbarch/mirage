7c0e43e194769edd1ee5ce6ca55952cabf441ac6
qwbarch <qwbarch@gmail.com>
2024-09-27 02:38:00 -0400

    Fix MirageCore packaging script

---
+++ package/mirage-core/README.md
@@ -0,0 +1,11 @@
+# MirageCore
+
+A set of dependencies for Mirage.
+
+This currently contains the following binaries:
+- [FSharp.Core](https://fsharp.org/) - 8.0
+- [FSharp.Control.AsyncSeq](https://fsprojects.github.io/FSharp.Control.AsyncSeq/) - 3.2.1
+- [FSharpPlus](https://github.com/fsprojects/FSharpPlus) - 1.5.0
+- [FSharpx.Async](https://github.com/fsprojects/FSharpx.Async) - 1.13.2
+
+Icon created by [IntegrityFate](https://integrityfate.com/).
--- package/mirage-core/script/package.bat
+++ package/mirage-core/script/package.bat
@@ -2,6 +2,7 @@
 
 pushd ..\..\silero-vad\script
 call build-lib.bat || exit /b
+call build.bat || exit /b
 popd
 
 call build.bat
@@ -19,13 +20,15 @@ copy %src%\FSharp.Control.AsyncSeq.dll .
 copy %src%\FSharp.Core.dll .
 copy %src%\FSharpPlus.dll .
 copy %src%\FSharpx.Async.dll .
-copy %src%\FSharpx.Collections.dll .
+rem copy %src%\FSharpx.Collections.dll .
 rem copy %src%\MathNet.Numerics.dll .
 rem copy %src%\MathNet.Numerics.FSharp.dll .
 
 rem Prepare SileroVAD files.
 mkdir SileroVAD
 pushd SileroVAD
+
+set src=..\..\..\..\..\..\silero-vad\bin
 
 copy ..\%src%\SileroVAD.API.dll .
 copy ..\%src%\SileroVAD.dll .
@@ -46,7 +49,7 @@ popd
 
 rem Move to the bin folder.
 popd
-pushd bin
+pushd ..\bin
 
 rem Create the Mirage.Core package.
 powershell Compress-Archive^
@@ -54,10 +57,8 @@ powershell Compress-Archive^
     -Path "Mirage.Core",^
           "../manifest.json",^
           "../icon.png",^
-          "../../../README.md",^
-          "../../../CHANGELOG.md",^
-          "../../../LICENSE"^
-    -DestinationPath "../bin/mirage.zip"
+          "../README.md"^
+    -DestinationPath "../bin/mirage-core.zip"
 
 
 popd
