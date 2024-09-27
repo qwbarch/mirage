f1d53b60cb50fdbcdb782fd9ab7be97eb3183565
qwbarch <qwbarch@gmail.com>
2024-09-27 17:04:12 -0400

    Re-add demo in README

--- package/mirage-lc/script/package.bat
+++ package/mirage-lc/script/package.bat
@@ -3,6 +3,7 @@
 call ./build.bat
 mkdir "../bin/BepInEx/plugins"
 powershell Move-Item -Path "../bin/Mirage.dll" -Destination "../bin/BepInEx/plugins/Mirage.dll"
+powershell Move-Item -Path "../bin/Mirage.Core.dll" -Destination "../bin/BepInEx/plugins/Mirage.Core.dll"
 powershell Compress-Archive^
     -Force^
     -Path "../bin/BepInEx",^
